cd258c7b89687d9e48381700346ab41f7ed62a46
tambj <soytbj@hotmail.com>
2023-05-19 21:16:55 +0200

    cambios

--- Producto4/app/routes/tasksRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tasksController = require('../controllers/TasksController');
-
-router.post('/tasks/:taskId/upload', tasksController.uploadFile);
-
-module.exports = router;
